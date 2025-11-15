ec4b2f203629b89cd1b84eb82daa41f483b490be
Vinicius Tutya <viniciustutya07@gmail.com>
2025-11-15 15:45:51 -0300

    feat: finalizando o js

--- script.js
+++ script.js
@@ -44,10 +44,88 @@ document.addEventListener("DOMContentLoaded", historicoBusca);
 
 // funcao para buscar dados na API
 async function buscarPais(nomePais){
+    const URL_API = 'https://restcountries.com/${nomePais}';
     
+    try {
+        const resposta = await fetch(URL_API);
 
+        if (!resposta.ok) {
+            throw new Error("Erro na requisição" + resposta.status);
+    }
+    
+    const dados = await resposta.json();
+    displayPaisInfo(dados[0]);
+    salvarHistorico(dados[0].name.common);
 
+} catch (error){
+    console.error("Erro ao buscar país:", error);
+    infoPais.innerHTML = "<p>País não encontrado. Tente novamente.</p>";
+    }
 }
+
+
+// funcao para exibir os dados no html 
+
+function displayPaisInfo(pais){
+    const populacao = pais.population.toLocaleString('pt-BR');
+
+    const capital = pais.capital ? pais.capital[0] : "N/A";
+
+    const infoHtml = `
+        <h3>${pais.name.common}</h3>
+        <img src="${pais.flags.png}" alt="Bandeira de ${pais.name.common}">
+        <p><strong>Capital:</strong> ${capital}</p>
+        <p><strong>População:</strong> ${populacao}</p>
+    `;
+
+    infoPais.innerHTML = infoHtml;
+}
+
+// funcao para gerenciar o historico de busca
+function pegarHistorico(){
+
+    const historicco = localStorage.getItem(KEY_LOCAL_STORAGE);
+
+    return historicco ? JSON.parse(historicco) : [];
+}
+
+
+// funcao para salvar um novo item no historico
+function salvarHistorico(nomePais){
+    const historico = pegarHistorico();
+
+    const filtrarHistorico = historico.filter(item => item.toLowerCase() !== nomePais.toLowerCase());
+
+    filtrarHistorico.unshift(nomePais);
+
+    const novoHistorico = filtrarHistorico.slice(0, 5);
+
+    localStorage.setItem(KEY_LOCAL_STORAGE, JSON.stringify(novoHistorico));
+
+    atualizarHistoricoDisplay();
+}
+
+// funcao para carregar o historico 
+function carregarHistorico(){
+
+    const historico = pegarHistorico();
+
+    historicoBusca.innerHTML = "";
+
+    historico.forEach(nomePais => {
+        const li = document.createElement("li");
+        li.textContent = nomePais;
+        historicoBusca.appendChild(li);
+    });
+}
+
+// funcao para limpar o historico
+function limparHistorico(){
+    localStorage.removeItem(KEY_LOCAL_STORAGE);
+    carregarHistorico();
+}
+
+
 
 
 
